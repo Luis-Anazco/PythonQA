57b5e3b09b61c2d081cae821019b37515f98c213
Luis-Anazco <luis.anazco.anazco@avanade.com>
2022-10-19 17:03:19 +0100

    Data types exercises

---
+++ file.py
@@ -0,0 +1,21 @@
+english = 50
+maths = 60
+science = 70
+
+total = english + maths + science
+average = total/3
+
+print('Your average score is:',average)
+
+eng = input('Please enter your score for English: ')
+mat = input('Please enter your score for Maths: ')
+sci = input('Please enter your score for Science: ')
+
+total = int(eng) + int(mat) + int(sci)
+average = total/3
+print('Your average score based on the results you provided is:',average)
+
+s=str('Hello World')
+i = int(3759)
+f = float(21.849)
+b = bool(False)
---
+++ strings.py
@@ -0,0 +1,10 @@
+from tkinter import N
+
+
+word1 = 'strings'
+word2 = 'are'
+word3 = 'fun'
+
+print(word1,word2,word3)
+print(word1 + word2 + word3)
+print(word1 + '\n' + word2 + '\n' + word3)
